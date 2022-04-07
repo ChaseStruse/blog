16a7863432270b93d1327ccb71570b530740e042
Chase Struse <chasestruse@gmail.ocm>
2022-04-07 15:11:43 -0500

    Got a start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
-
+import {BlogPost} from './components/blogpost/blogpost.jsx'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chase's Developer Logs</h1>
       </header>
+      <BlogPost />
     </div>
   );
 }
---
+++ src/components/blogpost/blogpost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function BlogPost() {
+  return (
+    <div className="Post">
+        <h3>Test Post</h3>
+        <p>Test post content</p>
+    </div>
+  );
+}
